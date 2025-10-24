ba71a331087797f8a6bf2eb104e8a6f27ff81271
Abdul S <asakho@ucsc.edu>
2025-10-23 23:48:43 -0700

    pl

--- Exercises/ex05.js
+++ Exercises/ex05.js
@@ -15,7 +15,7 @@ $("#needy-button").click(function () {
     $("body").css("background-color", colors[colorCount]);
 
     if ( colors[colorCount] == "Orchid" ) {
-        $("body").append("<img width=50 src='../Art101-New/Images/bucket-cat.png'>");
+        $("body").append("<img width=50 src='../Images/bucket-cat.png'>");
 }
 
 
